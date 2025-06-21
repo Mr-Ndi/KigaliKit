5e7cfc2b48be86a42d84901f091497acb70127b1
Mr-Ndi <onlythenotes@gmail.com>
2025-06-21 19:26:33 +0200

    Foundation: Stable syntaxy version

--- airtable/uploader.py
+++ airtable/uploader.py
@@ -3,6 +3,8 @@ import requests
 import os
 
 
+load_dotenv()
+
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 TABLE_NAME = os.getenv("AIRTABLE_TABLE_NAME")
@@ -10,7 +12,6 @@ TABLE_NAME = os.getenv("AIRTABLE_TABLE_NAME")
 if not AIRTABLE_API_KEY or not BASE_ID:
     raise ValueError("Please set AIRTABLE_API_KEY and BASE_ID in your .env file.")
 
-load_dotenv()
 def upload_to_airtable(jobs):
     url = f"https://api.airtable.com/v0/{BASE_ID}/{TABLE_NAME}"
     headers = {
