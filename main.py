d4ca92da54f44589a41864b2de424a74ab110c51
Mr-Ndi <onlythenotes@gmail.com>
2025-06-21 19:57:42 +0200

    Tracking the scrapping, uploading and site to be tracked in main function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
 from dotenv import load_dotenv
-
 from scraper.jobs import scrape_jobs
 from airtable.uploader import upload_to_airtable
 
 load_dotenv()
+
 def main():
     url = os.getenv("TARGET_URL")
+    if not url:
+        print("TARGET_URL not set in .env")
+        return
+
     print(f"Scraping jobs from: {url}")
-    
     jobs = scrape_jobs(url)
     print(f"Found {len(jobs)} jobs.")
 
