5c5d131a412da412c11e9f33563ca3b7d334b25a
Mr-Ndi <onlythenotes@gmail.com>
2025-08-03 21:43:53 +0200

    Working version

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
 api_key = os.getenv("AIRTABLE_API_KEY")
 base_id = os.getenv("AIRTABLE_BASE_ID")
-table_id = os.getenv("AIRTABLE_TABLE_NAME")
+table_name = os.getenv("AIRTABLE_TABLE_NAME")
 
-url = f"https://api.airtable.com/v0/{base_id}/{table_id}"
+url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
 headers = {
     "Authorization": f"Bearer {api_key}",
     "Content-Type": "application/json"
